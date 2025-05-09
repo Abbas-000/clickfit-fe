c69ca74fe00ba319d6affbee05952bb10000b87b
Ali Abbas <aliabbas000nochance@gmail.com>
2025-05-09 18:05:53 +0500

    drag and drop upload section

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 $(document).ready(function () {
-  $('#load-text').load('http://numbersapi.com/1/30/date?json', function(responseText, statusText, xhr) {
+  $('#load-text').load('http://numbersapi.com/1/30/date?json', function (responseText, statusText, xhr) {
     if (statusText == "error") {
       $('#load-text').text("Error loading text: " + xhr.status + " " + xhr.statusText);
     } else {
       $('#load-text').text(responseText);
     }
+  });
+
+  $(function () {
+    $("aks-file-upload").aksFileUpload({
+      fileUpload: "#file",
+      fileType: ["pdf", "docx", "rtf", "jpg", "jpeg", "png"],
+      dragDrop: true,
+      maxSize: "50 MB",
+      multiple: true,
+      maxFile: 5,
+      ajaxUpload: true,
+      ajax: {
+        directlyLoad: false,
+        url: "http://localhost:3000/upload",
+        type: "POST",
+        contentType: false,
+        processData: false,
+        cache: false,
+        async: true,
+        enctype: "multipart/form-data"
+      },
+    });
   });
 });
